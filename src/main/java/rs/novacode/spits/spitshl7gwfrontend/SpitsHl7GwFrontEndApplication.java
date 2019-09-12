20e6fe638d934a6dce504896291cc36a9e55973d
nikolaspiric <nikolaspiric86@gmail.com>
2019-09-12 14:02:56 +0200

    Initial commit

---
+++ src/main/java/rs/novacode/spits/spitshl7gwfrontend/SpitsHl7GwFrontEndApplication.java
@@ -0,0 +1,13 @@
+package rs.novacode.spits.spitshl7gwfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpitsHl7GwFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpitsHl7GwFrontEndApplication.class, args);
+	}
+
+}
